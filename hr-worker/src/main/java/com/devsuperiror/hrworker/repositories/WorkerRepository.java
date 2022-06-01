3773aca6c40c50ea7d3c9083b0908974b76f9add
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-05-31 21:04:23 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/devsuperiror/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperiror.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperiror.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+	
+	
+	
+	
+	
+}
