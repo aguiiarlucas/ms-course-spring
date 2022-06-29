5ccdc3455d492ce396362d33318f0437b34a3f07
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-06-29 15:45:43 -0300

    Hystix fault tolerance

--- hr-payroll/src/main/java/com/devsuperiror/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperiror/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,14 @@ package com.devsuperiror.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- hr-payroll/src/main/java/com/devsuperiror/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperiror/hrpayroll/resources/PaymentResource.java
@@ -9,21 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperiror.hrpayroll.entities.Payment;
 import com.devsuperiror.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
-@RequestMapping(value="/payments")
+@RequestMapping(value = "/payments")
 public class PaymentResource {
-
 
 	@Autowired
 	private PaymentService service;
-	
-	@GetMapping(value="/{workerId}/days/{days}")	
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+
+	public ResponseEntity<Payment> getPaymentAlternative( Long workerId,  Integer days){
+		Payment payment = new Payment("Brann", 400.0, days);
+		return ResponseEntity.ok(payment);
 	
+	}
+	
+
+
+
+
 	
 	
 }
