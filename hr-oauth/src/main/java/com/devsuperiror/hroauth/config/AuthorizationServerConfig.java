0bfef8634f2c9803ed0c0d4a3986403c181cd740
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-07-01 22:09:06 -0300

    JWT 2/2

--- hr-oauth/src/main/java/com/devsuperiror/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/devsuperiror/hroauth/config/AuthorizationServerConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 
 	@Autowired
-	private BCryptPasswordEncoder passwordEncode;
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private JwtAccessTokenConverter tokenConverter;
@@ -31,20 +31,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(security);
+		 security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(clients);
+		clients.inMemory()
+		.withClient("myappname123")
+		.secret(passwordEncoder.encode("myappsecret123"))
+		.scopes("read","write")
+		.authorizedGrantTypes("password")
+		.accessTokenValiditySeconds(86400);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(endpoints);
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore)
+		.accessTokenConverter(tokenConverter);
 	}
 
 	
