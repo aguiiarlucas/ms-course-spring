0f68a319102e07b0f359d338e2d090db3c1224d6
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-07-04 20:17:26 -0300

    DockerFile

--- hr-config-server/src/test/java/com/devsuperiror/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/devsuperiror/hrconfigserver/HrConfigServerApplicationTests.java
@@ -3,7 +3,9 @@ package com.devsuperiror.hrconfigserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.devsuperior.hrconfigserver.HrConfigServerApplication;
+
+@SpringBootTest(classes = HrConfigServerApplication.class)
 class HrConfigServerApplicationTests {
 
 	@Test
